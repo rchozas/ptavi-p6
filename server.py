0a04cdb953107985c5d1663d10a8f85d3a17a05d
Raquel Chozas <r.chozas@alumnos.urjc.es>
2017-11-25 23:55:54 +0100

    inic servidor

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -19,6 +20,17 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print("El cliente nos manda " + line.decode('utf-8'))
+            metodos = ["INVITE", "ACK", "BYE"]
+            metodo = line.decode('utf-8').split(' ')[0]
+            if len(line) >=2:
+                if metodo == "INVITE":
+                    enviar = b"SIP/2.0 100 Trying\r\n\r\n"
+                    enviar += b"SIP/2.0 180 Ring\r\n\r\n"
+                    enviar += b"SIP/2.0 200 OK\r\n\r\n"
+                    self.wfile.write(enviar)
+                elif metodo == "ACK":
+                    aEjecutar = './mp32rtp -i ' + IP + ' -p 23032 <' + FICHERO
+                    os.system(aEjecutar)
 
             # Si no hay más líneas salimos del bucle infinito
             if not line:
