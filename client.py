1c228b831b599b6d86c0dc9409008c8674ade0e3
Raquel Chozas <r.chozas@alumnos.urjc.es>
2017-11-27 19:40:03 +0100

    cliente

--- client.py
+++ client.py
@@ -7,16 +7,19 @@ Programa cliente que abre un socket a un servidor
 import socket
 import sys
 
+
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
 if len(sys.argv) != 3:
     sys.exit("Usage: python client.py method receiver@IP:SIPport")
-    
-METODO = sys.argv[1].upper()
-LOGIN = sys.argv[2].split("@")[0] + "@"
-IP = sys.argv[2].split("@")[1].split(":")[0]
-PUERTO = int(sys.argv[2].split('@')[1].split(":")[1])
+try:
+    METODO = sys.argv[1].upper()
+    LOGIN = sys.argv[2].split("@")[0] + "@"
+    IP = sys.argv[2].split("@")[1].split(":")[0]
+    PUERTO = int(sys.argv[2].split('@')[1].split(":")[1])
+except:
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
 
 # Contenido que vamos a enviar
 LINE = METODO + " sip:" + LOGIN + IP + " SIP/2.0\r\n"
@@ -28,10 +31,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((IP, PUERTO))
 
     print("Enviando: " + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    my_socket.send(bytes(LINE, 'utf-8') + b"\r\n")
     data = my_socket.recv(1024)
 
-    print('Recibido -- ', data.decode('utf-8'))
+    print('Recibido --')
+    print(data.decode('utf-8'))
     print("Terminando socket...")
+    
+# si recibe las respuestas 100 Trying, 180 Ringing, 200 OK    
+    recibe = data.decode('utf-8').split("\r\n\r\n")[0:-1]
+    if recibe == ["SIP/2.0 100 Trying", "SIP/2.0 180 Ringing", "SIP/2.0 200 OK"]:
+        LINEACK = "ACK sip:" + LOGIN + IP + " SIP/2.0\r\n"
+        print("Enviando: " + LINEACK)
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        #data = my_socket.recv(1024)
 
 print("Fin.")
